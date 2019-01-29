486f9da1083ea57ae0e831e7e993d970518fc17d
WhiteyHat <carlosroldan26396@gmail.com>
2019-01-29 17:46:12 +0000

    Update 1503248427885_user.js

--- database/migrations/1503248427885_user.js
+++ database/migrations/1503248427885_user.js
@@ -8,8 +8,7 @@ class UserSchema extends Schema {
     this.create('users', (table) => {
       table.increments()
       table.string('username', 80).notNullable().unique()
-      table.string('email', 254).notNullable().unique()
-      table.string('password', 60).notNullable()
+      table.string('session')
       table.timestamps()
     })
   }
