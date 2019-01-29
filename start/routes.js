950d842d63b77b7c084c803fa701d7a0781b01c7
WhiteyHat <carlosroldan26396@gmail.com>
2019-01-29 17:46:07 +0000

    Update routes.js

--- start/routes.js
+++ start/routes.js
@@ -4,6 +4,9 @@ const Route = use('Route')
 
 Route.on('/').render('welcome')
 
+Route.get('signup', 'LnurlController.createUser')
+Route.get('/withdraw/request', 'LnurlController.requestWithdrawal')
+
 // Withdrawal routes group
 Route.group(() => {
     Route.get('request', 'LnurlController.requestWithdrawal')
