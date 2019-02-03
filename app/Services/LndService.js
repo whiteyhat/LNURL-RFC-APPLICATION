ee2c1bc771769c620f2698ffabf2871ed5386eeb
WhiteyHat <carlosroldan26396@gmail.com>
2019-02-03 22:22:13 +0000

    Update LndService.js
    Create Service to perform payments to invoice requests

--- app/Services/LndService.js
+++ app/Services/LndService.js
@@ -2,6 +2,7 @@
 
 const lnService = require('ln-service')
 const Env = use('Env')
+const decodePaymentRequest = require('ln-service/decodePaymentRequest')
 
 let lndInstance = null
 
@@ -20,6 +21,25 @@ class LndService {
     getNodeAddress(){
         return `${Env.get('LND_HOST')}:${Env.get('LND_PORT')}`
     }
+
+    async payment(pr){
+    try {
+        const lnd = await getLndInstance()
+        // Returns the logged in user instance
+        const user = await auth.getUser()
+        const decodedPayReq = await decodePaymentRequest({ lnd, request: pr })
+        if (decodedPayReq.tokens > user.balance) {
+            return response.message({ error: "Not enough found" })
+        }
+        if (decodedPayReq.is_expired) {
+            return response.message({ error: "Invoice is expired" })
+        }
+        const invoicePaid = await payInvoice({ lnd, request: pr });
+
+    }catch(error){
+        Logger.error(error)
+    }
+}
 
 }
 
